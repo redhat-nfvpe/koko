6e55323f9d2650e4144634177acbc3116f8a4399
Tomofumi Hayashi <s1061123@gmail.com>
2017-05-31 10:46:45 +0900

    Make API separate from command.
    To support to call koko's code from other programs, API code is
    separated as a package, 'api'.

---
+++ api/koko_api.go
@@ -0,0 +1,249 @@
+/**
+ * koko: Container connector (api)
+ */
+package api
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/containernetworking/cni/pkg/ns"
+	"github.com/vishvananda/netlink"
+
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+)
+
+// VEth is a structure to descrive veth interfaces.
+type VEth struct {
+	NsName   string      // What's the network namespace?
+	LinkName string      // And what will we call the link.
+	IpAddr   []net.IPNet // Slice of IPv4/v6 adress.
+}
+
+// VxLan is a structure to descrive vxlan endpoint.
+type VxLan struct {
+	ParentIF string // parent interface name
+	Id       int    // VxLan ID
+	IpAddr   net.IP // VxLan destination address
+}
+
+// MakeVethPair makes veth pair and returns its link.
+func MakeVethPair(name, peer string, mtu int) (netlink.Link, error) {
+	veth := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  name,
+			Flags: net.FlagUp,
+			MTU:   mtu,
+		},
+		PeerName: peer,
+	}
+
+	if err := netlink.LinkAdd(veth); err != nil {
+		return nil, err
+	}
+	return veth, nil
+}
+
+// GetVethPair takes two link names and create a veth pair and return
+//both links.
+func GetVethPair(name1 string, name2 string) (link1 netlink.Link,
+	link2 netlink.Link, err error) {
+	link1, err = MakeVethPair(name1, name2, 1500)
+	if err != nil {
+		switch {
+		case os.IsExist(err):
+			err = fmt.Errorf(
+				"container veth name provided (%v) "+
+					"already exists", name1)
+			return
+		default:
+			err = fmt.Errorf("failed to make veth pair: %v", err)
+			return
+		}
+	}
+
+	link2, err = netlink.LinkByName(name2)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to lookup %q: %v\n", name2, err)
+	}
+
+	return
+}
+
+// AddVxLanInterface creates VxLan interface by given vxlan object
+func AddVxLanInterface(vxlan VxLan, devName string) error {
+	parentIF, err := netlink.LinkByName(vxlan.ParentIF)
+
+	if err != nil {
+		return fmt.Errorf("Failed to get %s: %v", vxlan.ParentIF, err)
+	}
+
+	vxlanconf := netlink.Vxlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:   devName,
+			TxQLen: 1000,
+		},
+		VxlanId:      vxlan.Id,
+		VtepDevIndex: parentIF.Attrs().Index,
+		Group:        vxlan.IpAddr,
+		Port:         4789,
+		Learning:     true,
+		L2miss:       true,
+		L3miss:       true,
+	}
+	err = netlink.LinkAdd(&vxlanconf)
+
+	if err != nil {
+		return fmt.Errorf("Failed to add vxlan %s: %v", devName, err)
+	}
+	return nil
+}
+
+// GetDockerContainerNS retrieves container's network namespace from
+// docker container id, given as containerID.
+func GetDockerContainerNS(containerID string) (namespace string, err error) {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	cli.UpdateClientVersion("1.24")
+
+	json, err := cli.ContainerInspect(ctx, containerID)
+	if err != nil {
+		err = fmt.Errorf("failed to get container info: %v", err)
+		return
+	}
+	if json.NetworkSettings == nil {
+		err = fmt.Errorf("failed to get container info: %v", err)
+		return
+	}
+	namespace = fmt.Sprintf("/proc/%d/ns/net", json.State.Pid)
+	return
+}
+
+// SetVethLink is low-level handler to set IP address onveth links given
+// a single VEth data object.
+// ...primarily used privately by makeVeth().
+func (veth *VEth) SetVethLink(link netlink.Link) (err error) {
+	var vethNs ns.NetNS
+
+	if veth.NsName == "" {
+		vethNs, err = ns.GetCurrentNS()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
+	} else {
+		vethNs, err = ns.GetNS(veth.NsName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
+	}
+
+	defer vethNs.Close()
+	if err = netlink.LinkSetNsFd(link, int(vethNs.Fd())); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+	}
+
+	err = vethNs.Do(func(_ ns.NetNS) error {
+		link, err := netlink.LinkByName(veth.LinkName)
+		if err != nil {
+			return fmt.Errorf("failed to lookup %q in %q: %v",
+				veth.LinkName, vethNs.Path(), err)
+		}
+
+		if err = netlink.LinkSetUp(link); err != nil {
+			return fmt.Errorf("failed to set %q up: %v",
+				veth.LinkName, err)
+		}
+
+		// Conditionally set the IP address.
+		for i := 0; i < len(veth.IpAddr); i++ {
+			addr := &netlink.Addr{IPNet: &veth.IpAddr[i], Label: ""}
+			if err = netlink.AddrAdd(link, addr); err != nil {
+				return fmt.Errorf(
+					"failed to add IP addr %v to %q: %v",
+					addr, veth.LinkName, err)
+			}
+		}
+
+		return nil
+	})
+
+	return
+}
+
+// RemoveVethLink is low-level handler to get interface handle in
+// container/netns namespace and remove it.
+func (veth *VEth) RemoveVethLink() (err error) {
+	var vethNs ns.NetNS
+
+	if veth.NsName == "" {
+		vethNs, err = ns.GetCurrentNS()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
+	} else {
+		vethNs, err = ns.GetNS(veth.NsName)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
+	}
+	defer vethNs.Close()
+
+	err = vethNs.Do(func(_ ns.NetNS) error {
+		link, err := netlink.LinkByName(veth.LinkName)
+		if err != nil {
+			return fmt.Errorf("failed to lookup %q in %q: %v",
+				veth.LinkName, vethNs.Path(), err)
+		}
+
+		err = netlink.LinkDel(link)
+		if err != nil {
+			return fmt.Errorf("failed to remove link %q in %q: %v",
+				veth.LinkName, vethNs.Path(), err)
+		}
+		return nil
+	})
+
+	return
+}
+
+// MakeVeth is top-level handler to create veth links given two VEth data
+// objects: veth1 and veth2.
+func MakeVeth(veth1 VEth, veth2 VEth) {
+
+	link1, link2, err := GetVethPair(veth1.LinkName, veth2.LinkName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+	}
+
+	veth1.SetVethLink(link1)
+	veth2.SetVethLink(link2)
+}
+
+// MakeVxLan makes vxlan interface and put it into container namespace
+func MakeVxLan(veth1 VEth, vxlan VxLan) {
+
+	err := AddVxLanInterface(vxlan, veth1.LinkName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "vxlan add failed: %v", err)
+	}
+
+	link, err2 := netlink.LinkByName(veth1.LinkName)
+	if err2 != nil {
+		fmt.Fprintf(os.Stderr, "Cannot get %s: %v", veth1.LinkName, err)
+	}
+	err = veth1.SetVethLink(link)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot add IPaddr/netns failed: %v",
+			err)
+	}
+}
+
+
+
--- koko_test.go
+++ koko_test.go
@@ -5,21 +5,23 @@ import (
 	"net"
 	"strings"
 	"testing"
+
+	"./api"
 )
 
 func TestParseLinkIPOption(t *testing.T) {
 	// test case1: parse "-n testns,testlink"
 	str1 := "testlink"
 	linkName1 := "testlink"
-	veth1 := vEth{}
+	veth1 := api.VEth{}
 
 	err1 := parseLinkIPOption(&veth1, strings.Split(str1, ","))
 	if err1 != nil {
 		t.Fatalf("Parse error: %v", err1)
 	}
-	if veth1.linkName != linkName1 {
+	if veth1.LinkName != linkName1 {
 		t.Fatalf("nsName Parse error %s should be %s",
-			veth1.linkName, linkName1)
+			veth1.LinkName, linkName1)
 	}
 
 	// test case2: parse "testlink,192.168.1.1/24"
@@ -27,23 +29,23 @@ func TestParseLinkIPOption(t *testing.T) {
 	linkName2 := "testlink"
 	linkIP4Addr2 := net.ParseIP("192.168.1.1")
 	linkIP4Prefix2 := net.CIDRMask(24, 32)
-	veth2 := vEth{}
+	veth2 := api.VEth{}
 
 	err2 := parseLinkIPOption(&veth2, strings.Split(str2, ","))
 	if err2 != nil {
 		t.Fatalf("Parse error: %v", err1)
 	}
-	if veth2.linkName != linkName2 {
+	if veth2.LinkName != linkName2 {
 		t.Fatalf("nsName Parse error %s should be %s",
-			veth2.nsName, linkName2)
+			veth2.NsName, linkName2)
 	}
-	if !veth2.ipAddr[0].IP.Equal(linkIP4Addr2) {
+	if !veth2.IpAddr[0].IP.Equal(linkIP4Addr2) {
 		t.Fatalf("ipAddr[0].IP Parse error %s should be %s",
-			veth2.ipAddr[0].IP, linkIP4Addr2)
+			veth2.IpAddr[0].IP, linkIP4Addr2)
 	}
-	if bytes.Compare(veth2.ipAddr[0].Mask, linkIP4Prefix2) != 0 {
+	if bytes.Compare(veth2.IpAddr[0].Mask, linkIP4Prefix2) != 0 {
 		t.Fatalf("ipAddr[0].Mask Parse error %s should be %s",
-			veth2.ipAddr[0].Mask, linkIP4Prefix2)
+			veth2.IpAddr[0].Mask, linkIP4Prefix2)
 	}
 
 	// test case3: parse "testlink,192.168.1.1/24,ff02::1/64"
@@ -53,31 +55,31 @@ func TestParseLinkIPOption(t *testing.T) {
 	linkIP4Prefix3 := net.CIDRMask(24, 32)
 	linkIP6Addr3 := net.ParseIP("ff02::1")
 	linkIP6Prefix3 := net.CIDRMask(64, 128)
-	veth3 := vEth{}
+	veth3 := api.VEth{}
 
 	err3 := parseLinkIPOption(&veth3, strings.Split(str3, ","))
 	if err3 != nil {
 		t.Fatalf("Parse error: %v", err1)
 	}
-	if veth3.linkName != linkName3 {
+	if veth3.LinkName != linkName3 {
 		t.Fatalf("nsName Parse error %s should be %s",
-			veth3.nsName, linkName3)
+			veth3.NsName, linkName3)
 	}
-	if !veth3.ipAddr[0].IP.Equal(linkIP4Addr3) {
+	if !veth3.IpAddr[0].IP.Equal(linkIP4Addr3) {
 		t.Fatalf("ipAddr[0].IP Parse error %s should be %s",
-			veth3.ipAddr[0].IP, linkIP4Addr3)
+			veth3.IpAddr[0].IP, linkIP4Addr3)
 	}
-	if bytes.Compare(veth3.ipAddr[0].Mask, linkIP4Prefix3) != 0 {
+	if bytes.Compare(veth3.IpAddr[0].Mask, linkIP4Prefix3) != 0 {
 		t.Fatalf("ipAddr[0].Mask Parse error %s should be %s",
-			veth3.ipAddr[0].Mask, linkIP4Prefix3)
+			veth3.IpAddr[0].Mask, linkIP4Prefix3)
 	}
-	if !veth3.ipAddr[1].IP.Equal(linkIP6Addr3) {
+	if !veth3.IpAddr[1].IP.Equal(linkIP6Addr3) {
 		t.Fatalf("ipAddr[1].IP Parse error %s should be %s",
-			veth3.ipAddr[1].IP, linkIP6Addr3)
+			veth3.IpAddr[1].IP, linkIP6Addr3)
 	}
-	if bytes.Compare(veth3.ipAddr[1].Mask, linkIP6Prefix3) != 0 {
+	if bytes.Compare(veth3.IpAddr[1].Mask, linkIP6Prefix3) != 0 {
 		t.Fatalf("ipAddr[1].Mask Parse error %s should be %s",
-			veth3.ipAddr[1].Mask, linkIP6Prefix3)
+			veth3.IpAddr[1].Mask, linkIP6Prefix3)
 	}
 
 	// test case4: parse "testlink,ff02::1/64"
@@ -85,23 +87,23 @@ func TestParseLinkIPOption(t *testing.T) {
 	linkName4 := "testlink"
 	linkIP6Addr4 := net.ParseIP("ff02::1")
 	linkIP6Prefix4 := net.CIDRMask(64, 128)
-	veth4 := vEth{}
+	veth4 := api.VEth{}
 
 	err4 := parseLinkIPOption(&veth4, strings.Split(str4, ","))
 	if err4 != nil {
 		t.Fatalf("Parse error: %v", err1)
 	}
-	if veth4.linkName != linkName4 {
+	if veth4.LinkName != linkName4 {
 		t.Fatalf("nsName Parse error %s should be %s",
-			veth4.nsName, linkName4)
+			veth4.NsName, linkName4)
 	}
-	if !veth4.ipAddr[0].IP.Equal(linkIP6Addr4) {
+	if !veth4.IpAddr[0].IP.Equal(linkIP6Addr4) {
 		t.Fatalf("ipAddr[0].IP Parse error %s should be %s",
-			veth4.ipAddr[0].IP, linkIP6Addr4)
+			veth4.IpAddr[0].IP, linkIP6Addr4)
 	}
-	if bytes.Compare(veth4.ipAddr[0].Mask, linkIP6Prefix4) != 0 {
+	if bytes.Compare(veth4.IpAddr[0].Mask, linkIP6Prefix4) != 0 {
 		t.Fatalf("ipAddr[0].Mask Parse error %s should be %s",
-			veth4.ipAddr[0].Mask, linkIP6Prefix4)
+			veth4.IpAddr[0].Mask, linkIP6Prefix4)
 	}
 
 }
