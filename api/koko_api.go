4e12fcfb862b26df84f9096a76d2deebcb3bf89d
Tomofumi Hayashi <s1061123@gmail.com>
2017-08-10 13:13:03 +0900

    Add port-mirroring (SPAN) feature into api
    Add port-mirroring feature, that monitors given target interface using
    linux TC feature.

--- api/koko_api.go
+++ api/koko_api.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"syscall"
 
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
@@ -19,7 +20,9 @@ import (
 type VEth struct {
 	NsName   string      // What's the network namespace?
 	LinkName string      // And what will we call the link.
-	IPAddr   []net.IPNet // Slice of IPv4/v6 address.
+	IPAddr   []net.IPNet // (optional) Slice of IPv4/v6 address.
+	MirrorEgress string  // (optional) source interface for egress mirror
+	MirrorIngress string // (optional) source interface for ingress mirror
 }
 
 // VxLan is a structure to descrive vxlan endpoint.
@@ -76,19 +79,18 @@ func GetVethPair(name1 string, name2 string) (link1 netlink.Link,
 		}
 	}
 
-	link2, err = netlink.LinkByName(name2)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to lookup %q: %v\n", name2, err)
+	if link2, err = netlink.LinkByName(name2); err != nil {
+		err = fmt.Errorf("Failed to lookup %q: %v\n", name2, err)
 	}
 
 	return
 }
 
 // AddVxLanInterface creates VxLan interface by given vxlan object
-func AddVxLanInterface(vxlan VxLan, devName string) error {
-	parentIF, err := netlink.LinkByName(vxlan.ParentIF)
+func AddVxLanInterface(vxlan VxLan, devName string) (err error) {
+	var parentIF netlink.Link
 
-	if err != nil {
+	if parentIF, err = netlink.LinkByName(vxlan.ParentIF); err != nil {
 		return fmt.Errorf("Failed to get %s: %v", vxlan.ParentIF, err)
 	}
 
@@ -114,10 +116,10 @@ func AddVxLanInterface(vxlan VxLan, devName string) error {
 }
 
 // AddVLanInterface creates VLan interface by given vlan object
-func AddVLanInterface(vlan VLan, devName string) error {
-	parentIF, err := netlink.LinkByName(vlan.ParentIF)
+func AddVLanInterface(vlan VLan, devName string) (err error) {
+	var parentIF netlink.Link
 
-	if err != nil {
+	if parentIF, err = netlink.LinkByName(vlan.ParentIF); err != nil {
 		return fmt.Errorf("Failed to get %s: %v", vlan.ParentIF, err)
 	}
 
@@ -128,19 +130,18 @@ func AddVLanInterface(vlan VLan, devName string) error {
 		},
 		VlanId: vlan.ID,
 	}
-	err = netlink.LinkAdd(&vlanconf)
 
-	if err != nil {
+	if err = netlink.LinkAdd(&vlanconf); err != nil {
 		return fmt.Errorf("Failed to add vlan %s: %v", devName, err)
 	}
 	return nil
 }
 
 // AddMacVLanInterface creates MacVLan interface by given macvlan object
-func AddMacVLanInterface(macvlan MacVLan, devName string) error {
-	parentIF, err := netlink.LinkByName(macvlan.ParentIF)
+func AddMacVLanInterface(macvlan MacVLan, devName string) (err error) {
+	var parentIF netlink.Link
 
-	if err != nil {
+	if parentIF, err = netlink.LinkByName(macvlan.ParentIF); err != nil {
 		return fmt.Errorf("Failed to get %s: %v", macvlan.ParentIF, err)
 	}
 
@@ -151,9 +152,8 @@ func AddMacVLanInterface(macvlan MacVLan, devName string) error {
 		},
 		Mode: macvlan.Mode,
 	}
-	err = netlink.LinkAdd(&macvlanconf)
 
-	if err != nil {
+	if err = netlink.LinkAdd(&macvlanconf); err != nil {
 		return fmt.Errorf("Failed to add vlan %s: %v", devName, err)
 	}
 	return nil
@@ -183,6 +183,187 @@ func GetDockerContainerNS(containerID string) (namespace string, err error) {
 	return
 }
 
+// SetIngressMirror sets TC to mirror ingress from given port
+// as MirrorIngress.
+func (veth *VEth) SetIngressMirror() (err error) {
+	var linkSrc, linkDest netlink.Link
+
+	if linkSrc, err = netlink.LinkByName(veth.MirrorIngress); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.MirrorIngress, veth.NsName, err)
+	}
+
+	if linkDest, err = netlink.LinkByName(veth.LinkName); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.LinkName, veth.NsName, err)
+	}
+
+	// tc qdisc add dev $SRC_IFACE ingress
+	qdisc := &netlink.Ingress{
+		QdiscAttrs: netlink.QdiscAttrs{
+			LinkIndex: linkSrc.Attrs().Index,
+			Handle: netlink.MakeHandle(0xffff, 0),
+			Parent: netlink.HANDLE_INGRESS,
+		},
+	}
+	if err = netlink.QdiscAdd(qdisc); err != nil {
+		if ! os.IsExist(err) {
+			return err
+		}
+	}
+
+	// tc filter add dev $SRC_IFACE parent fffff:
+	// protocol all
+	// u32 match u32 0 0
+	// action mirred egress mirror dev $DST_IFACE
+	filter := &netlink.U32 {
+		FilterAttrs: netlink.FilterAttrs {
+			LinkIndex: linkSrc.Attrs().Index,
+			Parent: netlink.MakeHandle(0xffff, 0),
+			Protocol: syscall.ETH_P_ALL,
+		},
+		Sel: &netlink.TcU32Sel{
+			Keys: []netlink.TcU32Key {
+				netlink.TcU32Key{
+					Mask: 0x0,
+					Val: 0,
+				},
+			},
+			Flags: netlink.TC_U32_TERMINAL,
+		},
+		Actions: []netlink.Action{
+			&netlink.MirredAction{
+				ActionAttrs: netlink.ActionAttrs{
+					Action: netlink.TC_ACT_PIPE,
+				},
+				MirredAction: netlink.TCA_EGRESS_MIRROR,
+				Ifindex: linkDest.Attrs().Index,
+			},
+		},
+	}
+
+	if err = netlink.FilterAdd(filter); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SetEgressMirror sets TC to mirror egress from given port
+// as MirrorEgress.
+func (veth *VEth) SetEgressMirror() (err error) {
+	var linkSrc, linkDest netlink.Link
+
+	if linkSrc, err = netlink.LinkByName(veth.MirrorEgress); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.MirrorEgress, veth.NsName, err)
+	}
+
+	if linkSrc.Attrs().TxQLen == 0 {
+		return fmt.Errorf("veth qlen must be non zero!")
+	}
+
+	if linkDest, err = netlink.LinkByName(veth.LinkName); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.LinkName, veth.NsName, err)
+	}
+
+	// tc qdisc add dev <SRC> handle 1: root prio
+	qdisc := netlink.NewPrio(
+		netlink.QdiscAttrs{
+			LinkIndex: linkSrc.Attrs().Index,
+			Handle: netlink.MakeHandle(1, 0),
+			Parent: netlink.HANDLE_ROOT,
+		})
+	if err = netlink.QdiscAdd(qdisc); err != nil {
+		if ! os.IsExist(err) {
+			return err
+		}
+	}
+	// tc filter add dev $SRC_IFACE parent 1:
+	// protocol all
+	// u32 match u32 0 0
+	// action mirred egress mirror dev $DST_IFACE
+	u32SelKeys := []netlink.TcU32Key {
+		netlink.TcU32Key{
+			Mask: 0x0,
+			Val: 0,
+		},
+	}
+	filter := &netlink.U32 {
+		FilterAttrs: netlink.FilterAttrs {
+			LinkIndex: linkSrc.Attrs().Index,
+			Parent: netlink.MakeHandle(1, 0),
+			Protocol: syscall.ETH_P_ALL,
+		},
+		Sel: &netlink.TcU32Sel{
+			Keys: u32SelKeys,
+			Flags: netlink.TC_U32_TERMINAL,
+		},
+		Actions: []netlink.Action{
+			&netlink.MirredAction{
+				ActionAttrs: netlink.ActionAttrs{
+					Action: netlink.TC_ACT_PIPE,
+				},
+				MirredAction: netlink.TCA_EGRESS_MIRROR,
+				Ifindex: linkDest.Attrs().Index,
+			},
+		},
+	}
+
+	if err = netlink.FilterAdd(filter); err != nil {
+		return err
+	}
+	return nil
+}
+
+// UnsetIngressMirror sets TC to mirror ingress from given port
+// as MirrorIngress.
+func (veth *VEth) UnsetIngressMirror() (err error) {
+	var linkSrc netlink.Link
+
+	if linkSrc, err = netlink.LinkByName(veth.MirrorIngress); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.MirrorIngress, veth.NsName, err)
+	}
+
+	// tc qdisc add dev $SRC_IFACE ingress
+	qdisc := &netlink.Ingress{
+		QdiscAttrs: netlink.QdiscAttrs{
+			LinkIndex: linkSrc.Attrs().Index,
+			Handle: netlink.MakeHandle(0xffff, 0),
+			Parent: netlink.HANDLE_INGRESS,
+		},
+	}
+	if err = netlink.QdiscDel(qdisc); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UnsetEgressMirror sets TC to mirror egress from given port
+// as MirrorEgress.
+func (veth *VEth) UnsetEgressMirror() (err error) {
+	var linkSrc netlink.Link
+
+	if linkSrc, err = netlink.LinkByName(veth.MirrorEgress); err != nil {
+		return fmt.Errorf("failed to lookup %q in %q: %v",
+			veth.MirrorEgress, veth.NsName, err)
+	}
+
+	// tc qdisc add dev <SRC> handle 1: root prio
+	qdisc := netlink.NewPrio(
+		netlink.QdiscAttrs{
+			LinkIndex: linkSrc.Attrs().Index,
+			Handle: netlink.MakeHandle(1, 0),
+			Parent: netlink.HANDLE_ROOT,
+		})
+	if err = netlink.QdiscDel(qdisc); err != nil {
+		return err
+	}
+	return nil
+}
+
 // SetVethLink is low-level handler to set IP address onveth links given
 // a single VEth data object.
 // ...primarily used privately by makeVeth().
@@ -190,20 +371,18 @@ func (veth *VEth) SetVethLink(link netlink.Link) (err error) {
 	var vethNs ns.NetNS
 
 	if veth.NsName == "" {
-		vethNs, err = ns.GetCurrentNS()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
+		if vethNs, err = ns.GetCurrentNS(); err != nil {
+			return fmt.Errorf("%v", err)
 		}
 	} else {
-		vethNs, err = ns.GetNS(veth.NsName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
+		if vethNs, err = ns.GetNS(veth.NsName); err != nil {
+			return fmt.Errorf("%v", err)
 		}
 	}
 
 	defer vethNs.Close()
 	if err = netlink.LinkSetNsFd(link, int(vethNs.Fd())); err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		return fmt.Errorf("%v", err)
 	}
 
 	err = vethNs.Do(func(_ ns.NetNS) error {
@@ -228,115 +407,188 @@ func (veth *VEth) SetVethLink(link netlink.Link) (err error) {
 			}
 		}
 
+		if veth.MirrorIngress != "" {
+			if err = veth.SetIngressMirror(); err != nil {
+				netlink.LinkDel(link)
+				return fmt.Errorf(
+					"Failed to set tc ingress mirror :%v",
+				err)
+			}
+		}
+		if veth.MirrorEgress != "" {
+			if err = veth.SetEgressMirror(); err != nil {
+				netlink.LinkDel(link)
+				return fmt.Errorf(
+					"Failed to set tc egress mirror: %v", err)
+			}
+		}
 		return nil
 	})
 
-	return
+	return err
 }
 
 // RemoveVethLink is low-level handler to get interface handle in
 // container/netns namespace and remove it.
 func (veth *VEth) RemoveVethLink() (err error) {
 	var vethNs ns.NetNS
+	var link netlink.Link
 
 	if veth.NsName == "" {
-		vethNs, err = ns.GetCurrentNS()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
+		if vethNs, err = ns.GetCurrentNS(); err != nil {
+			return fmt.Errorf("%v", err)
 		}
 	} else {
-		vethNs, err = ns.GetNS(veth.NsName)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
+		if vethNs, err = ns.GetNS(veth.NsName); err != nil {
+			return fmt.Errorf("%v", err)
 		}
 	}
 	defer vethNs.Close()
 
 	err = vethNs.Do(func(_ ns.NetNS) error {
-		link, err := netlink.LinkByName(veth.LinkName)
-		if err != nil {
+		if veth.MirrorIngress != "" {
+			if err = veth.UnsetIngressMirror(); err != nil {
+				return fmt.Errorf(
+					"Failed to unset tc ingress mirror :%v",
+				err)
+			}
+		}
+		if veth.MirrorEgress != "" {
+			if err = veth.UnsetEgressMirror(); err != nil {
+				return fmt.Errorf(
+					"Failed to unset tc egress mirror: %v",
+				err)
+			}
+		}
+
+		if link, err = netlink.LinkByName(veth.LinkName); err != nil {
 			return fmt.Errorf("failed to lookup %q in %q: %v",
 				veth.LinkName, vethNs.Path(), err)
 		}
 
-		err = netlink.LinkDel(link)
-		if err != nil {
+		if err = netlink.LinkDel(link); err != nil {
 			return fmt.Errorf("failed to remove link %q in %q: %v",
 				veth.LinkName, vethNs.Path(), err)
 		}
 		return nil
 	})
 
-	return
+	return err
 }
 
 // MakeVeth is top-level handler to create veth links given two VEth data
 // objects: veth1 and veth2.
-func MakeVeth(veth1 VEth, veth2 VEth) {
-
+func MakeVeth(veth1 VEth, veth2 VEth) (error) {
 	link1, link2, err := GetVethPair(veth1.LinkName, veth2.LinkName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
 	}
 
-	veth1.SetVethLink(link1)
-	veth2.SetVethLink(link2)
+	if err:= veth1.SetVethLink(link1); err != nil {
+		return err
+	}
+	if err:= veth2.SetVethLink(link2); err != nil {
+		return err
+	}
+	return nil
 }
 
 // MakeVxLan makes vxlan interface and put it into container namespace
-func MakeVxLan(veth1 VEth, vxlan VxLan) {
+func MakeVxLan(veth1 VEth, vxlan VxLan) (err error) {
+	var link netlink.Link
 
-	err := AddVxLanInterface(vxlan, veth1.LinkName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "vxlan add failed: %v", err)
+	if err = AddVxLanInterface(vxlan, veth1.LinkName); err != nil {
+		return fmt.Errorf("vxlan add failed: %v", err)
 	}
 
-	link, err2 := netlink.LinkByName(veth1.LinkName)
-	if err2 != nil {
-		fmt.Fprintf(os.Stderr, "Cannot get %s: %v", veth1.LinkName, err)
+	if link, err = netlink.LinkByName(veth1.LinkName); err != nil {
+		return fmt.Errorf("Cannot get %s: %v", veth1.LinkName, err)
 	}
-	err = veth1.SetVethLink(link)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot add IPaddr/netns failed: %v",
-			err)
+
+	if err = veth1.SetVethLink(link); err != nil {
+		return fmt.Errorf("Cannot add IPaddr/netns failed: %v", err)
 	}
+
+	if veth1.MirrorIngress != "" {
+		if err = veth1.SetIngressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc ingress mirror :%v",
+				err)
+		}
+	}
+	if veth1.MirrorEgress != "" {
+		if err = veth1.SetEgressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc egress mirror: %v", err)
+		}
+	}
+	return nil
 }
 
 // MakeVLan makes vlan interface
-func MakeVLan(veth1 VEth, vlan VLan) {
+func MakeVLan(veth1 VEth, vlan VLan) (err error) {
+	var link netlink.Link
 
-	err := AddVLanInterface(vlan, veth1.LinkName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "vlan add failed: %v", err)
+	if err = AddVLanInterface(vlan, veth1.LinkName); err != nil {
+		return fmt.Errorf("vlan add failed: %v", err)
 	}
 
-	link, err2 := netlink.LinkByName(veth1.LinkName)
-	if err2 != nil {
-		fmt.Fprintf(os.Stderr, "Cannot get %s: %v", veth1.LinkName, err)
+	if link, err = netlink.LinkByName(veth1.LinkName); err != nil {
+		return fmt.Errorf("Cannot get %s: %v", veth1.LinkName, err)
 	}
-	err = veth1.SetVethLink(link)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot add IPaddr/netns failed: %v",
-			err)
+	if err = veth1.SetVethLink(link); err != nil {
+		return fmt.Errorf("Cannot add IPaddr/netns failed: %v", err)
 	}
+
+	if veth1.MirrorIngress != "" {
+		if err = veth1.SetIngressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc ingress mirror :%v",
+				err)
+		}
+	}
+	if veth1.MirrorEgress != "" {
+		if err = veth1.SetEgressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc egress mirror: %v", err)
+		}
+	}
+	return nil
 }
 
 // MakeMacVLan makes macvlan interface
-func MakeMacVLan(veth1 VEth, macvlan MacVLan) {
+func MakeMacVLan(veth1 VEth, macvlan MacVLan) (err error) {
+	var link netlink.Link
 
-	err := AddMacVLanInterface(macvlan, veth1.LinkName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "macvlan add failed: %v", err)
+	if err = AddMacVLanInterface(macvlan, veth1.LinkName); err != nil {
+		return fmt.Errorf("macvlan add failed: %v", err)
 	}
 
-	link, err2 := netlink.LinkByName(veth1.LinkName)
-	if err2 != nil {
-		fmt.Fprintf(os.Stderr, "Cannot get %s: %v", veth1.LinkName, err)
+	if link, err = netlink.LinkByName(veth1.LinkName); err != nil {
+		return fmt.Errorf("Cannot get %s: %v", veth1.LinkName, err)
 	}
-	err = veth1.SetVethLink(link)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot add IPaddr/netns failed: %v",
-			err)
+
+	if err = veth1.SetVethLink(link); err != nil {
+		fmt.Errorf("Cannot add IPaddr/netns failed: %v", err)
 	}
+	if veth1.MirrorIngress != "" {
+		if err = veth1.SetIngressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc ingress mirror :%v",
+				err)
+		}
+	}
+	if veth1.MirrorEgress != "" {
+		if err = veth1.SetEgressMirror(); err != nil {
+			netlink.LinkDel(link)
+			return fmt.Errorf(
+				"Failed to set tc egress mirror: %v", err)
+		}
+	}
+	return err
 }
